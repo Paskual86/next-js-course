17e014b17352fd44a2477f5035e8a5ba3f84c303
Pablo Sebastian Andrada <pablosebastian.andrada@gmail.com>
2023-01-14 01:56:01 +0100

    Add Style for Event List

--- events-page-ts/components/events/EventItem.tsx
+++ events-page-ts/components/events/EventItem.tsx
@@ -1,8 +1,9 @@
+import Image from "next/image";
 import Link from "next/link";
 import Event from "../../Models/Event";
 
 const EventItem = (props: Event) => {
-    const {id, image, title, location, date} = props;
+    const {id, image, title, location, date, description} = props;
 
     const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
         day: 'numeric',
@@ -14,24 +15,31 @@ const EventItem = (props: Event) => {
 
     const exploreLink = `/events/${id}`;
     return (
-        <li>
-            <img src={`/${image}`} alt = {title}></img>
-
-            <div>
-                <div>
-                    <h2>{title}</h2>
-                    <div>
-                        <time>{humanReadableDate}</time>
+        <li className="m-2">
+            <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl" >
+                <div className="md:flex" >
+                    <div className="md:shrink-0" >
+                        <Image src={`/${image}`} alt = {title} className="h-48 w-full object-cover md:h-full md:w-48" width={200} height={200}></Image>
                     </div>
-                    <div>
-                        <address>{formattedAddress}</address>
+                    <div className="p-8" >
+                        <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold" >
+                            <figcaption className="font-medium">
+                                <div className="text-sky-500 dark:text-sky-400">
+                                    <Link href={exploreLink}>Explore Event</Link>
+                                </div>
+                            </figcaption>
+                        </div> 
+                        <h2 className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">{title}</h2>
+                        <p className="mt-2 text-slate-500">{description}</p>
+                        <div>
+                            <time>{humanReadableDate}</time>            
+                        </div>
+                        <div>
+                            <address>{formattedAddress}</address>
+                        </div>
                     </div>
-                </div>
-                <div>
-                    <Link href={exploreLink}>Explore Event</Link>
                 </div>
             </div>
-
         </li>
     )
 }
