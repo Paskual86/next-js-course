f3179e8ad2d0761553345549fbe1b95abfdf7cf5
Pablo Sebastian Andrada <pablosebastian.andrada@gmail.com>
2023-01-22 13:32:46 +0100

    Add stilyng headers

--- events-page-ts/components/layout/main-header.tsx
+++ events-page-ts/components/layout/main-header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 import classes from './main-header.module.css'
 const MainHeader = () => {
     return <header className={classes.header}>
-        <div className={classes.logo}>
+        <div className="title text-2xl font-black text-teal-500">
             <Link href="/">Next Event</Link>
         </div>
         <nav className={classes.navigation} >
