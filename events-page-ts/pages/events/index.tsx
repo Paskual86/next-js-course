982772d6391fd228ab27cf7bfea3d7740d32ca79
Pablo Sebastian Andrada <pablosebastian.andrada@gmail.com>
2023-01-22 16:00:17 +0100

    Adding Filter Form & Navigating to the Filtered Events

--- events-page-ts/pages/events/index.tsx
+++ events-page-ts/pages/events/index.tsx
@@ -1,7 +1,21 @@
+import { useRouter } from "next/router";
+import EventSearch from "../../components/events/event-search";
+import EventList from "../../components/events/EventList";
+import { getAllEvents } from "../../dummy-data";
+
 const HomeEvent = () => {
-    return <div>
-        <h1>All Event</h1>
-    </div>
+    const router = useRouter();
+    const events = getAllEvents();
+
+    const filteredEventSearchHandler = (year: string, month: string) => {
+        const fullPath = `/events/${year}/${month}`;
+        router.push(fullPath);
+    }
+
+    return <>
+        <EventSearch onFilterEvent={filteredEventSearchHandler}></EventSearch>
+        <EventList items={events} ></EventList>
+    </>
 }
 
 export default HomeEvent;
