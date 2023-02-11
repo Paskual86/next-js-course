dd2388b52556caa432bc7063efa40acf2ca847ea
Pablo Sebastian Andrada <pablosebastian.andrada@gmail.com>
2023-02-11 16:11:41 +0100

    Add Firebase connection

--- events-page-ts/api/eventEndpoints.ts
+++ events-page-ts/api/eventEndpoints.ts
@@ -2,46 +2,62 @@ import { DUMMY_EVENTS } from "../dummy-data";
 import { databases } from "../firebase/realtimedatabase";
 import DateFilter from "../Models/DateFilter";
 import { ref, child, get } from "firebase/database";
-const WORK_WITH_DUMMY_DATA = true;
+import Event from "../Models/Event";
 
-export function getFeaturedEvents() {
+const WORK_WITH_DUMMY_DATA = false;
+
+async function _getAllEvents():Promise<Event[]> {
+  const dbRef = ref(databases);
+  const events = await get(child(dbRef, `events`));
+  if (events.exists()){
+    var obj = events.val()
+    let result:  Event[] = [];
+    for (let key in obj){
+      result.push({
+        id: key,
+        date: obj[key].date,
+        description: obj[key].description,
+        title: obj[key].title,
+        image: obj[key].image,
+        isFeatured: obj[key].isFeatured,
+        location: obj[key].location
+      })
+    }
+    return result.filter((event) => event.isFeatured);
+  }else{
+    return [];
+  }
+}
+
+export async function getFeaturedEvents() {
     if (WORK_WITH_DUMMY_DATA) return DUMMY_EVENTS.filter((event) => event.isFeatured);
+    else {
+          return (await _getAllEvents()).filter((event) => event.isFeatured);
+        }
   }
   
-  export function getAllEvents() {
+  
+  export async function getAllEvents() {
     if (WORK_WITH_DUMMY_DATA) return DUMMY_EVENTS;
     else{
-        const dbRef = ref(databases);
-        get(child(dbRef, `events`)).then((snapshot) => {
-            if (snapshot.exists()) {
-              console.log(snapshot.val());
-            } else {
-              console.log("No data available");
-            }
-          }).catch((error) => {
-            console.error(error);
-          });
+        return await _getAllEvents();
     }
   }
   
-  export function getFilteredEvents(dateFilter: DateFilter) {
+  export async function getFilteredEvents(dateFilter: DateFilter) {
     const { year, month } = dateFilter;
+    const eventsData = WORK_WITH_DUMMY_DATA ? DUMMY_EVENTS : await _getAllEvents();
+    let filteredEvents = eventsData.filter((event) => {
+        const eventDate = new Date(event.date);
+        
+        return (
+            eventDate.getFullYear() === year && eventDate.getMonth() === month - 1
+        );
+    });
+    return filteredEvents;
+}
   
-    if (WORK_WITH_DUMMY_DATA) {
-        let filteredEvents = DUMMY_EVENTS.filter((event) => {
-            const eventDate = new Date(event.date);
-            
-            return (
-                eventDate.getFullYear() === year && eventDate.getMonth() === month - 1
-            );
-        });
-        return filteredEvents;
-    }
-  }
-  
-  export function getEventById(id: string) {
-    if (WORK_WITH_DUMMY_DATA) {
-        return DUMMY_EVENTS.find((event) => event.id === id);
-    }
-
+  export async function getEventById(id: string) {
+    const eventsData = WORK_WITH_DUMMY_DATA ? DUMMY_EVENTS : await _getAllEvents();
+        return eventsData.find((event) => event.id === id);
   }
--- events-page-ts/pages/index.tsx
+++ events-page-ts/pages/index.tsx
@@ -1,12 +1,25 @@
 import EventList from "../components/events/EventList";
 import { getFeaturedEvents } from "../api/eventEndpoints";
+import Event from "../Models/Event";
 
-const HomePage = () => {
-    const featureEvents = getFeaturedEvents();
-
+const HomePage = (props:{events:Event[]}) => {
+    if (!props.events){
+        return <p>Loading</p>
+    }
     return <div>
-        <EventList items={featureEvents!} ></EventList>
+        <EventList items={props.events!} ></EventList>
     </div>
 }
 
 export default HomePage;
+
+export async function getStaticProps() {
+    const featureEvents = await getFeaturedEvents();
+    console.log('getStaticProps');
+    console.log(featureEvents);
+    return {
+        props:{
+            events: featureEvents
+        }
+    }
+}
